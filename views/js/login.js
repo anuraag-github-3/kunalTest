58c8c2e87bd20ab5bc41dc9cb9b7c2dcbc59c79d
kunalmkv <kunalm4.kvtagore@gmail.com>
2023-02-06 10:27:00 +0530

    login (frontend + backend API)

--- views/js/login.js
+++ views/js/login.js
@@ -1,8 +1,4 @@
-
-
-const host = 'localhost';
-const protocol = 'http';
-const port = 3000;
+const apiUrl = 'http://localhost:3000';
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 
@@ -18,10 +14,9 @@ sign_in_btn.addEventListener('click', () => {
 
 })
 
-
-
-async function SignIn(event) {
+async function handleSignInSubmit(event) {
     event.preventDefault();
+
     const userName = event.target.userName.value;
     const email = event.target.email.value;
     const phone = event.target.phone.value;
@@ -32,17 +27,40 @@ async function SignIn(event) {
         email,
         phone,
         password
-    }
-    console.log('hii', newUser);
+    };
+
     try {
-        await axios.post(`${protocol}://${host}:${port}/newUser/signin`, newUser).then(response => {
+        const response = await axios.post(`${apiUrl}/newUser/signin`, newUser);
 
+        // If the server returns a success message, show an alert with the message
+        if (response.data.message) {
             alert(response.data.message);
-        })
+        }
+    } catch (err) {
+        // If the server returns an error, show an alert with the error message
+        alert(err.response.data.message || 'User already exists! Please sign in or try a different email address');
     }
-    catch (err) {
+}
 
-        alert("User already exists!! Login or try another EmailID");
+
+async function handleLoginSubmit(event) {
+    event.preventDefault();
+
+    const mail = event.target.mail.value;
+    const password = event.target.password.value;
+    const loginDetail = { mail, password };
+
+    try {
+        const response = await axios.post(`${apiUrl}/user/login`, loginDetail);
+
+        if (response.status === 200) {
+            alert(response.data.message);
+
+        }
+    } catch (err) {
+        const errorMessage = err.response?.data.message || "Incorrect Credentials! Please try again";
+        alert(errorMessage);
+        console.error("Error during login", JSON.stringify(err));
+        document.body.innerHTML += `<div style="color:red;"> ${err.message} <div>`;
     }
-
 }
